ca41214aa53654c5ab363fd42cc44c0ad4ebe5c9
Rashel124 <webcoderitrashel124@gmail.com>
2025-02-09 23:54:35 -0800

    Problem Solved using

---
+++ script.js
@@ -0,0 +1,62 @@
+
+
+
+// let x = 30;
+// var y = 40;
+// // let x = 40;
+
+// let amount;
+// var number;
+
+// const chocolate = 40;
+
+// // console.log("Hello Everyone");
+
+// amount = 60;
+// number = 4;
+// // var result = x+y;
+// // console.log(result);
+
+// // let finalAmount = amount*number;
+
+// // console.log(finalAmount);
+
+// // let chocolatePrice2 = chocolate*2;
+// // console.log(chocolatePrice2);
+
+
+
+
+// amount = 80;
+
+// number = 2;
+
+// chocolate = 50;
+
+// // let updateAmount = amount*number;
+
+// // console.log(updateAmount);
+
+// // let updatedChocolate2 = chocolate*2;
+
+// // console.log(updatedChocolate2);
+
+
+
+let totalMoney = 500;
+let egg = 5;
+let kitkat =2;
+
+let eachEgg =15;
+let eachKitkat =40;
+
+let eggPrice = egg*eachEgg;
+
+let kitkatPrice = kitkat*eachKitkat;
+
+let totalPrice=  eggPrice+kitkatPrice;
+
+let remainingMoney = totalMoney-totalPrice;
+
+console.log(remainingMoney);
+
